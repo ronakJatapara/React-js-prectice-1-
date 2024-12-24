85bb0ead164055fd62aec2dada07a5353b7029da
JAVASCRIPT2 <ronakjatapara76@gmail.com>
2024-12-24 13:29:52 +0530

    first commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+   <h2>ronak jatapara</h2>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+   <h1>
+    my self ronak jatapara | i am from botad
+   </h1>
+  )
+}
+
+export default App
